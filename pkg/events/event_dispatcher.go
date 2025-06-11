21af36b14bc4d89fc31c3ca3b84a373613e8aff5
Joao Caetano <joao.remonato@grupoagtech.com>
2025-06-11 17:54:20 -0300

    Implement EventDispatcher with registration and handler management

---
+++ pkg/events/event_dispatcher.go
@@ -0,0 +1,41 @@
+package events
+
+import "errors"
+
+var ErrHandlerAlreadyExistis = errors.New("handler já esta registrado")
+
+type EventDispatcher struct {
+	handlers map[string][]EventHandlerInterface
+}
+
+func NewEventDispatcher() *EventDispatcher {
+	return &EventDispatcher{handlers: make(map[string][]EventHandlerInterface)}
+}
+
+func (ed *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
+	if _, ok := ed.handlers[eventName]; ok {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				return ErrHandlerAlreadyExistis
+			}
+		}
+	}
+
+	ed.handlers[eventName] = append(ed.handlers[eventName], handler)
+	return nil
+}
+
+func (ed *EventDispatcher) Clear() {
+	ed.handlers = make(map[string][]EventHandlerInterface)
+}
+
+func (ed *EventDispatcher) Has(eventName string, handler EventHandlerInterface) bool {
+	if _, ok := ed.handlers[eventName]; ok {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				return true
+			}
+		}
+	}
+	return false
+}
